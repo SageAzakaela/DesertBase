34d08648bc6f1f7c36f3cd90b28389e1c91021e5
SageAzakaela <sageazakaela@gmail.com>
2022-12-14 00:42:59 -0700

    Added Vanilla Building Pool
    located in the buildings folder!

--- Lots/Aza's Area - Podunk/Objects/spawnpoints.lua
+++ Lots/Aza's Area - Podunk/Objects/spawnpoints.lua
@@ -1,7 +1,10 @@
 function SpawnPoints()
 return {
   unemployed = {
-    { worldX = 0, worldY = 9, posX = 61, posY = 216, posZ = 0 }
+    { worldX = 0, worldY = 8, posX = 125, posY = 219, posZ = 0 },
+    { worldX = 0, worldY = 8, posX = 84, posY = 219, posZ = 0 },
+    { worldX = 0, worldY = 8, posX = 272, posY = 204, posZ = 0 },
+    { worldX = 0, worldY = 8, posX = 155, posY = 183, posZ = 0 }
   }
 }
 end
