18fec717640198005674f2e86bf8d43cab7ccbda
SageAzakaela <sageazakaela@gmail.com>
2022-12-15 13:24:51 -0700

    Did more Work on Podunk -- Added a railway
    just like it says.
    
    IF YOU HAVE PROBLEMS UPDATING, MAKE SURE YOU STASH YOUR CHANGES, then UPDATE, THEN!!! VERY IMPORTANT, RECOVER YOUR CHANGES.

--- Lots/Aza's Area - Podunk/Objects/spawnpoints.lua
+++ Lots/Aza's Area - Podunk/Objects/spawnpoints.lua
@@ -1,10 +1,16 @@
 function SpawnPoints()
 return {
   unemployed = {
-    { worldX = 0, worldY = 8, posX = 125, posY = 219, posZ = 0 },
-    { worldX = 0, worldY = 8, posX = 84, posY = 219, posZ = 0 },
-    { worldX = 0, worldY = 8, posX = 272, posY = 204, posZ = 0 },
-    { worldX = 0, worldY = 8, posX = 155, posY = 183, posZ = 0 }
+    { worldX = 0, worldY = 8, posX = 105, posY = 40, posZ = 0 },
+    { worldX = 0, worldY = 8, posX = 50, posY = 42, posZ = 0 },
+    { worldX = 0, worldY = 8, posX = 34, posY = 55, posZ = 0 },
+    { worldX = 0, worldY = 8, posX = 115, posY = 179, posZ = 0 },
+    { worldX = 0, worldY = 8, posX = 115, posY = 165, posZ = 0 },
+    { worldX = 0, worldY = 8, posX = 135, posY = 219, posZ = 0 },
+    { worldX = 0, worldY = 8, posX = 273, posY = 204, posZ = 0 },
+    { worldX = 0, worldY = 8, posX = 241, posY = 181, posZ = 0 },
+    { worldX = 0, worldY = 8, posX = 271, posY = 265, posZ = 0 },
+    { worldX = 0, worldY = 8, posX = 237, posY = 25, posZ = 0 }
   }
 }
 end
